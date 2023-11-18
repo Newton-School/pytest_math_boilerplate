f397d8cdf0b76a949a434678d494eb0bd5852236
Surendra Puri Goswami <surendraiitkgp111@gmail.com>
2023-11-18 22:15:08 +0530

    int

---
+++ nst_math.py
@@ -0,0 +1,22 @@
+e = 2.718281828459045
+pi = 3.141592653589793
+
+
+def perm(n, k):
+	# write your code here
+	pass
+	
+	
+def comb(n, k):
+	# write your code here
+	pass
+
+
+def gcd(*integers):
+	# write your code here
+	pass
+
+
+def nearest_prime(number):
+	# write your code here
+	pass
